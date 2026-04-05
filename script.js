1a1d6320e9643812ad1f367e83f105298ffc5024
Glenda Joyce Reed <100007624+Glenda9031@users.noreply.github.com>
2026-04-05 15:49:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,140 +22,143 @@ loadSong(songs[songIndex]);
 
 // Update song details
 function loadSong(song) {
-    title.innerText = song;
-    audio.src = `music/${song}.mp3`;
-    cover.src = `images/${song}.jpg`;
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
 }
 
 // Play song
 function playSong() {
-    musicContainer.classList.add('play');
-    playBtn.querySelector('i.fas').classList.remove('fa-play');
-    playBtn.querySelector('i.fas').classList.add('fa-pause');
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
 
-    audio.play();
+  audio.play();
 }
 
 // Pause song
 function pauseSong() {
-    musicContainer.classList.remove('play');
-    playBtn.querySelector('i.fas').classList.add('fa-play');
-    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
 
-    audio.pause();
+  audio.pause();
 }
 
 // Previous song
 function prevSong() {
-    songIndex--;
+  songIndex--;
 
-    if (songIndex < 0) {
-        songIndex = songs.length - 1;
-    }
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
 
-    loadSong(songs[songIndex]);
+  loadSong(songs[songIndex]);
 
-    playSong();
+  playSong();
 }
 
 // Next song
 function nextSong() {
-    songIndex++;
+  songIndex++;
 
-    if (songIndex > songs.length - 1) {
-        songIndex = 0;
-    }
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
 
-    loadSong(songs[songIndex]);
+  loadSong(songs[songIndex]);
 
-    playSong();
+  playSong();
 }
 
 // Update progress bar
 function updateProgress(e) {
-    const { duration, currentTime } = e.srcElement;
-    const progressPercent = (currentTime / duration) * 100;
-    progress.style.width = `${progressPercent}%`;
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
 }
 
 // Set progress bar
 function setProgress(e) {
-    const width = this.clientWidth;
-    const clickX = e.offsetX;
-    const duration = audio.duration;
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
 
-    audio.currentTime = (clickX / width) * duration;
+  audio.currentTime = (clickX / width) * duration;
 }
 
-// Get duration & currentTime for Time of song
-function DurTime(e) {
-    const { duration, currentTime } = e.srcElement;
-    let sec;
-    let sec_d;
+//get duration & currentTime for Time of song
+function DurTime (e) {
+	const {duration,currentTime} = e.srcElement;
+	var sec;
+	var sec_d;
 
-    // Define minutes currentTime
-    let min = (currentTime==null) ? 0 :
-    Math.floor(currentTime / 60);
-    min = min < 10 ? '0' + min : min;
+	// define minutes currentTime
+	let min = (currentTime==null)? 0:
+	 Math.floor(currentTime/60);
+	 min = min <10 ? '0'+min:min;
 
-    // Define seconds currentTime
-    function get_sec(x) {
-        if (Math.floor(x) >= 60) {
-            for (var i = 1; i <= 60; i++) {
-                if (Math.floor(x) >= (60 * i) && 
-                Math.floor(x) < (60 * (i + 1))) {
-                    sec = Math.floor(x) - (60 * i);
-                    sec = sec < 10 ? '0' + sec : sec;
-                }
-            }
-        } else {
-            sec = Math.floor(x);
-            sec = sec < 10 ? '0' + sec : sec;
-        }
-    }
+	// define seconds currentTime
+	function get_sec (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec = Math.floor(x) - (60*i);
+					sec = sec <10 ? '0'+sec:sec;
+				}
+			}
+		}else{
+		 	sec = Math.floor(x);
+		 	sec = sec <10 ? '0'+sec:sec;
+		 }
+	} 
 
-    get_sec(currentTime, sec);
+	get_sec (currentTime,sec);
 
-    // Change currentTime DOM
-    currTime.innerHTML = min + ':' + sec;
+	// change currentTime DOM
+	currTime.innerHTML = min +':'+ sec;
 
-    // Define minutes duration
-    let min_d = (isNaN(duration) === true) ? '0' :
-    Math.floor(duration / 60);
-    min_d = min_d < 10 ? '0' + min_d : min_d;
+	// define minutes duration
+	let min_d = (isNaN(duration) === true)? '0':
+		Math.floor(duration/60);
+	 min_d = min_d <10 ? '0'+min_d:min_d;
 
-    function get_sec_d(x) {
-        if (Math.floor(x) >= 60) {
-            for (var i = 1; i <= 60; i++) {
-                if (Math.floor(x) >= (60 * i) && 
-                Math.floor(x) < (60 * (i + 1))) {
-                    sec_d = Math.floor(x) - (60 * i);
-                    sec_d = sec_d < 10 ? '0' + sec_d : sec_d;
-                }
-            }
-        } else {
-            sec_d = (isNaN(duration) === true) ? '0' :
-            Math.floor(x);
-            sec_d = sec_d < 10 ? '0' + sec_d : sec_d;
-        }
-    }
 
-    // Define seconds duration
-    get_sec_d(duration);
+	 function get_sec_d (x) {
+		if(Math.floor(x) >= 60){
+			
+			for (var i = 1; i<=60; i++){
+				if(Math.floor(x)>=(60*i) && Math.floor(x)<(60*(i+1))) {
+					sec_d = Math.floor(x) - (60*i);
+					sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+				}
+			}
+		}else{
+		 	sec_d = (isNaN(duration) === true)? '0':
+		 	Math.floor(x);
+		 	sec_d = sec_d <10 ? '0'+sec_d:sec_d;
+		 }
+	} 
 
-    // Change duration DOM
-    durTime.innerHTML = min_d + ':' + sec_d;
+	// define seconds duration
+	
+	get_sec_d (duration);
+
+	// change duration DOM
+	durTime.innerHTML = min_d +':'+ sec_d;
+		
 };
 
 // Event listeners
 playBtn.addEventListener('click', () => {
-    const isPlaying = musicContainer.classList.contains('play');
+  const isPlaying = musicContainer.classList.contains('play');
 
-    if (isPlaying) {
-        pauseSong();
-    } else {
-        playSong();
-    }
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
 });
 
 // Change song
@@ -171,5 +174,5 @@ progressContainer.addEventListener('click', setProgress);
 // Song ends
 audio.addEventListener('ended', nextSong);
 
-// Time of song update
-audio.addEventListener('timeupdate', DurTime);
+// Time of song
+audio.addEventListener('timeupdate',DurTime);
